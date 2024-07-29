8fa2a1a65efba7b56ec0227162e5b0c41e498781
Pruthvirajsinh Solanki <pruthvirajmsolanki@gmail.com>
2024-07-29 16:55:46 -0400

    addded files

---
+++ PizzaSQL.sql
@@ -0,0 +1,138 @@
+-- Retrieve the total number of orders placed.
+SELECT 
+    COUNT(order_id) AS total_orders
+FROM
+    orders;
+    
+-- Calculate the total revenue generated from pizza sales.
+SELECT 
+    ROUND(SUM(order_details.qunatity * pizzas.price),
+            2) AS total_revenue
+FROM
+    order_details
+        JOIN
+    pizzas ON order_details.pizza_id = pizzas.pizza_id;
+    
+-- Identify the highest-priced pizza.
+SELECT 
+    MAX(pizzas.price) AS price, pizza_types.name
+FROM
+    pizzas
+        JOIN
+    pizza_types ON pizzas.pizza_type_id = pizza_types.pizza_type_id
+GROUP BY pizza_types.name
+ORDER BY price DESC
+LIMIT 1;
+
+-- Identify the most common pizza size ordered.
+SELECT 
+    pizzas.size, COUNT(order_details.qunatity) AS Total_Orders
+FROM
+    pizzas
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY Total_Orders DESC;
+
+-- List the top 5 most ordered pizza types along with their quantities.  
+SELECT 
+    pizza_types.name, SUM(order_details.qunatity) AS Total
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY Total DESC
+LIMIT 5;
+
+-- Join the necessary tables to find the total quantity of each pizza category ordered.
+SELECT 
+    pizza_types.category,
+    SUM(order_details.qunatity) AS quantity
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizza_types.category
+ORDER BY quantity DESC;
+
+-- Determine the distribution of orders by hour of the day.
+SELECT 
+    HOUR(order_time) AS Hours, COUNT(order_id) AS Orders
+FROM
+    orders
+GROUP BY Hours;
+    
+-- Join relevant tables to find the category-wise distribution of pizzas.
+SELECT 
+    category, COUNT(name)
+FROM
+    pizza_types
+GROUP BY category;
+
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+
+SELECT 
+    ROUND(AVG(quantity))
+FROM
+    (SELECT 
+        orders.order_date, SUM(order_details.qunatity) AS quantity
+    FROM
+        orders
+    JOIN order_details ON orders.order_id = order_details.order_id
+    GROUP BY orders.order_date) AS orders_quantity;
+    
+-- Determine the top 3 most ordered pizza types based on revenue.
+SELECT 
+    pizza_types.name,
+    SUM(order_details.qunatity * pizzas.price) AS revenue
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY revenue DESC
+LIMIT 3;
+
+-- Advanced:
+-- Calculate the percentage contribution of each pizza type to total revenue.
+SELECT 
+    pizza_types.category,
+    (SUM(order_details.quantity * pizzas.price) / (SELECT 
+            ROUND(SUM(order_details.qunatity * pizzas.price),
+                        2) AS total_sales
+        FROM
+            order_details
+                JOIN
+            pizzas ON pizzas.pizza_id = order_details.pizza_id)) * 100 AS revenue
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    orders_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category
+ORDER BY revenue DESC
+;
+-- Analyze the cumulative revenue generated over time.
+
+select order_date,sum(revenue) over(order by order_date) as cum_revenue from
+(SELECT 
+    SUM(order_details.qunatity * pizzas.price) AS revenue,
+    orders.order_date
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id
+    join orders on orders.order_id=order_details.order_id
+    group by orders.order_date) as sales;
+    
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+
